536a127b242b00549e5ab32db5e36a663407fb35
Joi-lctrl <anyw2002@gmail.com>
2026-04-22 01:03:17 +0800

    Remove cleanup scripts; add GA result .mat
    Delete two host-specific cleanup batch scripts in APDLRESULTS_MTO/task5 (cleanup-ansys-LAPTOP-TOPLMTNP-3448.bat and cleanup-ansys-LAPTOP-TOPLMTNP-6308.bat). Add a new binary GA result file MTO/ga_single_20260421_223222.mat (single-run output).

--- APDLRESULTS_MTO/task5/cleanup-ansys-LAPTOP-TOPLMTNP-6308.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set LOCALHOST=%COMPUTERNAME%
-if /i "%LOCALHOST%"=="LAPTOP-TOPLMTNP" (taskkill /f /pid 6572)
-if /i "%LOCALHOST%"=="LAPTOP-TOPLMTNP" (taskkill /f /pid 6308)
-
-del /F cleanup-ansys-LAPTOP-TOPLMTNP-6308.bat
